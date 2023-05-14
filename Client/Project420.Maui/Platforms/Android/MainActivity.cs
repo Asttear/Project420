787ae94d570a0d39e4d1235d756d36cdbc2b4365
Asttear <asttear@sina.cn>
2023-05-15 01:42:33 +0800

    适配安卓主题颜色

--- Client/Project420.Maui/Platforms/Android/MainActivity.cs
+++ Client/Project420.Maui/Platforms/Android/MainActivity.cs
@@ -4,8 +4,14 @@ using Android.OS;
 
 namespace Project420.Maui
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Window.SetStatusBarColor(Android.Graphics.Color.White);
+            Window.SetNavigationBarColor(Android.Graphics.Color.White);
+            base.OnCreate(savedInstanceState);
+        }
     }
 }
